b37b14cc25417c2b94b8e34e58377aaa8965071e
Putra-pkwl03 <putrapongkowulu@gmail.com>
2025-12-03 09:34:57 +0700

    menyesuikan konfigurasi ath midleware sctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,26 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class
+        ]);
+
+        $middleware->append(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Unauthorized. Token tidak valid atau sudah logout.'
+                ], 401);
+            }
+        });
+    })
+
+    ->create();
