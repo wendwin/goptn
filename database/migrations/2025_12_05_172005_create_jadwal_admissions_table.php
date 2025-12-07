55ad6064c0792b73a549bd4d4237af162062e3a8
Putra-pkwl03 <putrapongkowulu@gmail.com>
2025-12-08 00:05:27 +0700

    migrasi submission update

--- database/migrations/2025_12_05_172005_create_jadwal_admissions_table.php
+++ database/migrations/2025_12_05_172005_create_jadwal_admissions_table.php
@@ -11,12 +11,18 @@ return new class extends Migration
         Schema::create('jadwal_admissions', function (Blueprint $table) {
             $table->id();
             $table->enum('type', ['national', 'mandiri']);
+            $table->enum('category', ['snbp', 'snbt', 'mandiri'])->nullable();
             $table->foreignId('campus_id')->nullable()->constrained('campuses')->nullOnDelete();
+
             $table->string('name');
-            $table->date('start_date');
-            $table->date('end_date');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+
+            $table->unsignedInteger('batch')->nullable();
+
             $table->string('status')->default('pending');
             $table->text('description')->nullable();
+            
             $table->timestamps();
         });
     }
